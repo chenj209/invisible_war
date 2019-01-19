1104337d3166176d3532650e3d91c003680fe351
chenj209 <kinderjianda.chen@gmail.com>
2019-01-19 12:24:15 -0500

    .

---
+++ Invisible War/Assets/Scripts/Door.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "player")
+        {
+            Debug.Log("Ding Ding Ding");
+        }
+    }
+}
