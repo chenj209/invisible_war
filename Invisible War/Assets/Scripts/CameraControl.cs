4309a15558d51209a9de405918f5a71521218f35
chenj209 <kinderjianda.chen@gmail.com>
2019-01-19 15:41:46 -0500

    demo

--- Invisible War/Assets/Scripts/CameraControl.cs
+++ Invisible War/Assets/Scripts/CameraControl.cs
@@ -10,13 +10,17 @@ public class CameraControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+        }
     }
 
     private void FixedUpdate()
