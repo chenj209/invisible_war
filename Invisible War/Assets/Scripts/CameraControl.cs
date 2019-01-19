f2b9db8c388d871e0edf22db72470b1535c59c8b
Tony <tonycl.wu@mail.utoronto.ca>
2019-01-19 14:34:44 -0500

    Add basic character movement

---
+++ Invisible War/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public float rotSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // X-axis respect rotation.
+        float xRot = -Input.GetAxis("Mouse Y");
+        this.transform.Rotate(Vector3.right, xRot * rotSpeed);
+    }
+}
---
+++ Invisible War/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveForce;
+    public float rotSpeed;
+
+    private Rigidbody playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //Vector3 move = playerBody.velocity;
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+        //playerBody.AddForce(this.transform.right * xMove * moveForce);
+        //playerBody.AddForce(this.transform.forward * zMove * moveForce);
+        Vector3 move = this.transform.right * xMove * moveForce;
+        move += this.transform.forward * zMove * moveForce;
+        playerBody.velocity = move;
+
+        // Y-axis respect rotation.
+        float yRot = Input.GetAxis("Mouse X");
+        this.transform.Rotate(Vector3.up, yRot * rotSpeed);
+    }
+}
