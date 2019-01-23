c61a1b3905d13c5ebaee72021967e557cb038711
tonycl.wu@mail.utoronto.ca <tonycl.wu@mail.utoronto.ca>
2019-01-23 18:57:38 -0500

    add different moving pattern and jump function for the player

--- Invisible War/Assets/Scripts/PlayerControl.cs
+++ Invisible War/Assets/Scripts/PlayerControl.cs
@@ -4,36 +4,80 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public enum MovePattern
+    {
+        Walking,
+        Running,
+        Sneaking
+    };
+    public MovePattern moveMode;
+
     public float moveForce;
-    public float rotSpeed;
 
     private Rigidbody playerBody;
+
+    public bool isGround;
+    public float jumpSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         playerBody = this.GetComponent<Rigidbody>();
+        moveMode = MovePattern.Walking;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveMode = MovePattern.Running;
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            moveMode = MovePattern.Sneaking;
+        }
+        else
+        {
+            moveMode = MovePattern.Walking;
+        }
+        move();
+
+        isGround = Physics.Raycast(transform.position, -transform.up, 1.5f);
+        if (isGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerBody.velocity += transform.up * jumpSpeed;
+        }
+    }
+
+    private void move()
+    {
+        float moveSpeed = moveForce;
+        switch (moveMode) {
+            case MovePattern.Walking:
+                moveSpeed = moveForce;
+                break;
+            case MovePattern.Running:
+                moveSpeed = 2 * moveForce;
+                break;
+            case MovePattern.Sneaking:
+                moveSpeed = .6f * moveForce;
+                break;
+        }
+
         //Vector3 move = playerBody.velocity;
         float xMove = Input.GetAxis("Horizontal");
         float zMove = Input.GetAxis("Vertical");
         //playerBody.AddForce(this.transform.right * xMove * moveForce);
         //playerBody.AddForce(this.transform.forward * zMove * moveForce);
-        Vector3 move = this.transform.right * xMove * moveForce;
-        move += this.transform.forward * zMove * moveForce;
+        Vector3 move = this.transform.right * xMove * moveSpeed;
+        move += this.transform.forward * zMove * moveSpeed;
         playerBody.velocity = move;
-
-        // Y-axis respect rotation.
-        float yRot = Input.GetAxis("Mouse X");
-        this.transform.Rotate(Vector3.up, yRot * rotSpeed);
     }
 }
