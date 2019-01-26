61d442345140cc1e570d7ae0ab127da76ba5c945
Kennnyoung <kyh649782182@gmail.com>
2019-01-26 13:18:04 -0500

    added bullet damage and player status

---
+++ Invisible War/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletDamage;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerStatus ps = collision.gameObject.GetComponent("PlayerStatus") as PlayerStatus;
+            ps.GetHit();
+        }
+        Destroy(gameObject);
+    }
+}
