5dd94121b35a9ec52997c9ce39738f8e556b7012
TonyUUU <TonyUUU@github.com>
2019-02-03 14:54:46 -0500

    Add rotate effect for the UI text

---
+++ Invisible War/Assets/Scripts/UISpiner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISpiner : MonoBehaviour
+{
+
+    public string playerID;
+
+    private GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.Find("Player" + playerID + "/PlayerCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(transform.position * 2 - camera.transform.position);
+    }
+}
