8a6c9a59b0cfa01d66ca08a4ed3074c0c8ce6cb4
Ken Young <kyh649782182@gmail.com>
2019-02-26 20:29:15 -0500

    added shoot bot

---
+++ Invisible War/Assets/Scripts/TriggerShooting.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerShooting : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject shootBot;
+    public Text instruction;
+    public GameObject panel;
+    public GameObject cam;
+    private bool firstTime = false;
+    private bool rotate = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotate)
+        {
+            var rotation = Quaternion.LookRotation(shootBot.transform.position - player.transform.position);
+            player.transform.rotation = Quaternion.Slerp(player.transform.rotation, rotation, Time.deltaTime * 10);
+        }
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !firstTime)
+        {
+            PlayerControl pc = player.GetComponent<PlayerControl>();
+            pc.enabled = false;
+        
+            yield return new WaitForSeconds(1);
+            cam.transform.eulerAngles = new Vector3(0, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
+            rotate = true;
+          
+            yield return new WaitForSeconds(2);
+            shootBot.transform.LookAt(player.transform);
+            rotate = false;
+            BotShoot bs = shootBot.GetComponent<BotShoot>();
+            bs.shoot = true;
+            panel.SetActive(true);
+            instruction.text = "You got hit by Hunter! \n Location exposed! \n Run for life!";
+            firstTime = true;
+        }
+    }
+
+    private IEnumerator OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            yield return new WaitForSeconds(1);
+            panel.SetActive(false);
+        }
+
+    }
+}
