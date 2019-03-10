ac66554188e04394e41c912812f7149f7b975c7a
Shuyue Wang <k1631111@kcl.ac.uk>
2019-03-09 20:50:17 -0500

    add rules

--- Invisible War/Assets/Scripts/Controllers/TutorialStateController.cs
+++ Invisible War/Assets/Scripts/Controllers/TutorialStateController.cs
@@ -19,6 +19,17 @@ public class TutorialStateController : MonoBehaviour
     public static bool HunterShootDone = false;
     public static bool GhostFreezeDone = false;
 
+    public GameObject hunter;
+    public GameObject ghost;
+    public Canvas canvas;
+    public Canvas tutorialCanvas;
+    public Image background1;
+    public Image background2;
+    public Text p1R;
+    public Text p2R;
+    public Text blink1;
+    public Text blink2;
+
     public Text p1S;
     public Text p2S;
     public float delay;
@@ -28,6 +39,8 @@ public class TutorialStateController : MonoBehaviour
     private string player1State;
     private string player2State;
     private bool isBlinking;
+    private bool blinking1;
+    private bool blinking2;
 
     private Animator an;
 
@@ -36,19 +49,53 @@ public class TutorialStateController : MonoBehaviour
     {
         p1S.text = "";
         p2S.text = "";
-        StartCoroutine(PopUp("Enter Tutorial1", p1S));
-        player1State = "tutorial1";
-        StartCoroutine(PopUp("Enter Tutorial1", p2S));
-        player2State = "tutorial1";
+        p1R.text = "";
+        p2R.text = "";
         isBlinking = true;
+        blinking1 = true;
+        blinking2 = true;
         an = gameObject.GetComponent<Animator>();
+        player1State = "rule1";
+        player2State = "rule1";
+        p1R.text = "Welcome to Invisible Tag!";
+        p2R.text = "Welcome to Invisible Tag!";
+        StartCoroutine(Blink1());
+        StartCoroutine(Blink2());
+        DisablePlayers();
+        HideTutorial();
     }
 
     // Update is called once per frame
     void Update()
     {
         // for player1
-        if (player1State == "tutorial1" && HunterTutOne)
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (player1State == "rule1")
+            {
+                p1R.text = "This is a player versus player game.\n" +
+                    "Your goal is to catch the invisible ghost " +
+                    "controlled by the other player in the arena " +
+                    "within two minutes time limit.";
+                player1State = "rule2";
+            }
+            else if (player1State == "rule2")
+            {
+                p1R.text = "There are total three rounds and the player " +
+                    "who wins the most rounds will win the game.";
+                player1State = "rule3";
+            }
+            else if (player1State == "rule3")
+            {
+                p1R.text = "Now lets start with teaching you the basic machanics in the game.";
+                player1State = "rule4";
+            }
+            else if (player1State == "rule4")
+            {
+                StartTutorial(1);
+            }
+        }
+        else if (player1State == "tutorial1" && HunterTutOne)
         { 
             StartCoroutine(PopUp("Enter Tutorial2", p1S));
             player1State = "tutorial2";
@@ -77,29 +124,122 @@ public class TutorialStateController : MonoBehaviour
         }
 
         // for player2
-            if (player2State == "tutorial1" && GhostTutOne)
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (player2State == "rule1")
             {
-                StartCoroutine(PopUp("Enter Tutorial2", p2S));
-                player2State = "tutorial2";
+                p2R.text = "This is a player versus player game.\n" +
+                    "Your goal is to avoid being caught by the invisible hunter " +
+                    "controlled by the other player in the arena " +
+                    "for two minutes.";
+                player2State = "rule2";
             }
-            else if (player2State == "tutorial2" && GhostTutTwo)
+            else if (player2State == "rule2")
             {
-                StartCoroutine(PopUp("Enter Tutorial3", p2S));
-                player2State = "tutorial3";
+                p2R.text = "There are total three rounds and the player " +
+                    "who wins the most rounds will win the game.";
+                player2State = "rule3";
             }
-            else if (player2State == "tutorial3" && GhostTutThree)
+            else if (player2State == "rule3")
             {
-                player2State = "stand by";
-                if (player1State != "stand by")
-                {
-                    StartCoroutine(Flash("Waiting for Player1", p2S));
-                }
-                else
-                {
-                    isBlinking = false;
-                }
+                p2R.text = "Now lets start with teaching you the basic machanics in the game.";
+                player2State = "rule4";
+            }
+            else if (player2State == "rule4")
+            {
+                StartTutorial(2);
+            }
         }
-        
+        else if (player2State == "tutorial1" && GhostTutOne)
+        {
+            StartCoroutine(PopUp("Enter Tutorial2", p2S));
+            player2State = "tutorial2";
+        }
+        else if (player2State == "tutorial2" && GhostTutTwo)
+        {
+            StartCoroutine(PopUp("Enter Tutorial3", p2S));
+            player2State = "tutorial3";
+        }
+        else if (player2State == "tutorial3" && GhostTutThree)
+        {
+            player2State = "stand by";
+            if (player1State != "stand by")
+            {
+                StartCoroutine(Flash("Waiting for Player1", p2S));
+            }
+            else
+            {
+                isBlinking = false;
+            }
+        }
+    }
+
+    private void HideTutorial()
+    {
+        canvas.transform.GetChild(0).gameObject.SetActive(false);
+        canvas.transform.GetChild(1).gameObject.SetActive(false);
+        tutorialCanvas.transform.GetChild(0).gameObject.SetActive(false);
+        tutorialCanvas.transform.GetChild(1).gameObject.SetActive(false);
+    }
+
+    private void ShowTutorial(int player)
+    {
+        if (player == 1)
+        {
+            canvas.transform.GetChild(0).gameObject.SetActive(true);
+            tutorialCanvas.transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else if (player == 2)
+        {
+            canvas.transform.GetChild(1).gameObject.SetActive(true);
+            tutorialCanvas.transform.GetChild(1).gameObject.SetActive(true);
+        }
+    }
+
+    private void DisablePlayers()
+    {
+        hunter.GetComponent<PlayerControl>().enabled = false;
+        hunter.GetComponent<shooting>().enabled = false;
+        hunter.GetComponent<CatchPlayer>().enabled = false;
+        ghost.GetComponent<PlayerControl>().enabled = false;
+        ghost.GetComponent<Freeze>().enabled = false;
+    }
+
+    private void ActivePlayer(int player)
+    {
+        if (player == 1)
+        {
+            hunter.GetComponent<PlayerControl>().enabled = true;
+            hunter.GetComponent<shooting>().enabled = true;
+            hunter.GetComponent<CatchPlayer>().enabled = true;
+        }
+        else if (player == 2)
+        {
+            ghost.GetComponent<PlayerControl>().enabled = true;
+            ghost.GetComponent<Freeze>().enabled = true;
+        }
+    }
+
+    void StartTutorial(int player)
+    {
+         ShowTutorial(player);
+         ActivePlayer(player);
+         if (player == 1)
+         {
+            background1.enabled = false;
+            p1R.enabled = false;
+            blinking1 = false;
+            StartCoroutine(PopUp("Enter Tutorial1", p1S));
+            player1State = "tutorial1";
+         }
+         else if (player == 2)
+         {
+            background2.enabled = false;
+            p2R.enabled = false;
+            blinking2 = false;
+            StartCoroutine(PopUp("Enter Tutorial1", p2S));
+            player2State = "tutorial1";
+         }
     }
 
     IEnumerator PopUp(string state, Text text)
@@ -120,6 +260,30 @@ public class TutorialStateController : MonoBehaviour
             yield return new WaitForSeconds(0.5f);
         }
         an.SetTrigger("FadeOut");
+    }
+
+    IEnumerator Blink1()
+    {
+        while (blinking1)
+        {
+            blink1.enabled = false;
+            yield return new WaitForSeconds(0.5f);
+            blink1.enabled = true;
+            yield return new WaitForSeconds(0.5f);
+        }
+        blink1.enabled = false;
+    }
+
+    IEnumerator Blink2()
+    {
+        while (blinking2)
+        {
+            blink2.enabled = false;
+            yield return new WaitForSeconds(0.5f);
+            blink2.enabled = true;
+            yield return new WaitForSeconds(0.5f);
+        }
+        blink2.enabled = false;
     }
 
     public void OnFadeComplete()
