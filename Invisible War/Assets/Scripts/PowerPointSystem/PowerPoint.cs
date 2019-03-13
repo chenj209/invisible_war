7e654a114f127c3c11ddc2798e6433d362562ee5
tonycl.wu@mail.utoronto.ca <tonycl.wu@mail.utoronto.ca>
2019-03-13 17:27:23 -0400

    add powerpoint system

---
+++ Invisible War/Assets/Scripts/PowerPointSystem/PowerPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerPoint : MonoBehaviour
+{
+    public GameObject controlSystem;
+    public GameObject ghost;
+    public GameObject hunter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
