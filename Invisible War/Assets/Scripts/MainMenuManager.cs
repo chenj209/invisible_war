bcf99acea5aa46750a7b9033ea270385927fe860
tonycl.wu@mail.utoronto.ca <tonycl.wu@mail.utoronto.ca>
2019-03-13 15:21:02 -0400

    fixed mainmenu bug

--- Invisible War/Assets/Scripts/MainMenuManager.cs
+++ Invisible War/Assets/Scripts/MainMenuManager.cs
@@ -17,6 +17,8 @@ public class MainMenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.visible=true;
+        Cursor.lockState = CursorLockMode.None;
         curState = State.Start;
         startPannel.SetActive(true);
         menuPannel.SetActive(false);
