744eafd1bb836602eb6e6be26c0f64165f9c8a02
tonycl.wu@mail.utoronto.ca <tonycl.wu@mail.utoronto.ca>
2019-03-19 20:49:57 -0400

    add GhostSeal system. the first feature reset has been tested

---
+++ Invisible War/Assets/Scripts/SealSystem/SealSystem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Random = System.Random;
+
+public class SealSystem : MonoBehaviour
+{
+    public List<GameObject> seals;
+    public GameObject ghost;
+
+    private Random rand;
+
+    // For test purpose.
+    public float resetTimer;
+    private float curTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rand = new Random();
+        ResetSys();
+
+        // For test purpose.
+        curTime = resetTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (curTime <= 0f)
+        {
+            ResetSys();
+            curTime = resetTimer;
+        } else
+        {
+            curTime -= Time.deltaTime;
+        }
+    }
+
+    // Reset the system : Tested.
+    void ResetSys()
+    {
+        int activeSeals = 5;
+        int totalSeals = seals.Count;
+        foreach (GameObject seal in seals)
+        {
+            if (rand.Next(totalSeals) < activeSeals)
+            {
+                // This seal is picked as activate.
+                seal.SetActive(true);
+                activeSeals--;
+            } else
+            {
+                // This seal is not picked.
+                seal.SetActive(false);
+            }
+            totalSeals--;
+        }
+    }
+}
